ea56fb9945dfc86ee822f9767768f1b4c74f0c0e
tshepi_tech <tshepi.tech@gmail.com>
2022-03-23 20:21:03 +0100

    Imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+//NPM Packages
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router";
+
+//Project Files
+import Home from "./pages/Home";
+import OrderList from "./pages/OrderList";
+
 export default function App() {
   return (
     <div className="App">
--- src/pages/OrderList.jsx
+++ src/pages/OrderList.jsx
@@ -1,5 +1,10 @@
 //NPM Packages
 import React from "react";
+import { useNavigate, useParams } from "react-router";
+
+//Project Files
+import progress from "../Data/progress.json";
+import OrderItem from "../components/OrderItem";
 
 export default function OrderList() {
   return <div>OrderList</div>;
