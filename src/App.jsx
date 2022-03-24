ae7fe4b45bcfa7c730e97b209e8594ccb1b2f3ec
tshepi_tech <tshepi.tech@gmail.com>
2022-03-24 16:08:21 +0100

    Errorscreen screen & status state created

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,43 @@ import { Routes, Route } from "react-router";
 //Project Files
 import Home from "./pages/Home";
 import OrderList from "./pages/OrderList";
+import ErrorScreen from "./screens/ErrorScreen";
 
 export default function App() {
   //Local state
   const [orders, setOrders] = useState([]);
+  const [status, setStatus] = useState(0); // 0: loading, 1: loaded, 2: error.
 
   //Properties
   const url = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
 
   //Methods
 
-  useEffect(() => {
-    loadOrders();
-  }, []);
+  useEffect(() => loadOrders(url, setOrders, setStatus), []);
 
-  const loadOrders = async () => {
-    const data = await fetch(url);
+  async function loadOrders(url, setState, setStatus) {
+    try {
+      const response = await fetch(url);
+      const orders = await response.json();
 
-    const orders = await data.json();
-    setOrders(orders);
-  };
+      setState(orders);
+      setStatus(1);
+    } catch (error) {
+      console.error(`Cannot load the todos from ${url}. Error: ${error}`);
+      setStatus(2);
+    }
+  }
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        {status === 1 && <Route path="/" element={<Home />} />}
         <Route
           path="/progress/:progressId"
           element={<OrderList orders={orders} />}
         />
       </Routes>
+      {status === 2 && <ErrorScreen />}
     </div>
   );
 }
---
+++ src/screens/ErrorScreen.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ErrorScreen() {
+  return <h1>Data could not be loaded. Please refresh or try again later</h1>;
+}
