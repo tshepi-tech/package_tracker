e496890948f0f7beb452f3fe08185404415b8484
tshepi_tech <tshepi.tech@gmail.com>
2022-03-25 13:37:21 +0100

    css homepage done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="hero">
+      <h2 className="header">InstaPacket</h2>
       <div className="hero_content">
-        <h1>The best and smoothest app for your orders</h1>
+        <h1>The best and most efficient service for your orders</h1>
       </div>
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,6 @@ export default function Home() {
 
   return (
     <section>
-      <Header />
       <Hero />
       <div>{Progress}</div>
     </section>
