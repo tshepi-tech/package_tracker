d86b1b28193543fbacb54aaf27328e19ae083a1a
tshepi_tech <tshepi.tech@gmail.com>
2022-03-25 21:13:31 +0100

    Header elements removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 //Project Files
 import progress from "../Data/progress.json";
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import ProgressItem from "../components/ProgressItem";
 
